a6f46633e9dac69b478d001965f0147d7ac9e19e
Prokudin <prokus@a483e705f5d5.ant.amazon.com>
2019-12-14 14:04:52 +0100

    readme updated

--- bps_demos/train_modelnet_conv3d.py
+++ bps_demos/train_modelnet_conv3d.py
@@ -190,11 +190,11 @@ def main():
     start = time.time()
     for epoch_idx in pbar:
         fit(model, DEVICE, train_loader, optimizer)
-        if epoch_idx == 100:
+        if epoch_idx == 700:
             for param_group in optimizer.param_groups:
                 print("decreasing the learning rate to 1e-4..")
                 param_group['lr'] = 1e-4
-        if epoch_idx == 1000:
+        if epoch_idx == 1200:
             for param_group in optimizer.param_groups:
                 print("decreasing the learning rate to 1e-4..")
                 param_group['lr'] = 1e-5
