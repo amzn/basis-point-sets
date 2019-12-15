6d8dcf5fd0f61d173f58c04e5262e72e3c82de40
Prokudin <prokus@a483e705f5d5.ant.amazon.com>
2019-12-15 19:05:48 +0100

    readme updated

--- bps_demos/train_modelnet_conv3d.py
+++ bps_demos/train_modelnet_conv3d.py
@@ -179,7 +179,7 @@ def main():
 
     optimizer = pt.optim.Adam(model.parameters(), lr=1e-3)
 
-    n_epochs = 500
+    n_epochs = 1000
     pbar = range(0, n_epochs)
     test_accs = []
     test_losses = []
@@ -190,18 +190,18 @@ def main():
     start = time.time()
     for epoch_idx in pbar:
         fit(model, DEVICE, train_loader, optimizer)
-        if epoch_idx == 200:
+        if epoch_idx == 800:
             for param_group in optimizer.param_groups:
                 print("decreasing the learning rate to 1e-4..")
                 param_group['lr'] = 1e-4
-        if epoch_idx == 300:
+        if epoch_idx == 900:
             for param_group in optimizer.param_groups:
                 print("decreasing the learning rate to 1e-5..")
                 param_group['lr'] = 1e-5
-        if epoch_idx == 400:
-            for param_group in optimizer.param_groups:
-                print("decreasing the learning rate to 1e-6..")
-                param_group['lr'] = 1e-6
+        # if epoch_idx == 400:
+        #     for param_group in optimizer.param_groups:
+        #         print("decreasing the learning rate to 1e-6..")
+        #         param_group['lr'] = 1e-6
         if epoch_idx % 1 == 0:
             test_loss, test_acc = test(model, DEVICE, test_loader, epoch_idx)
             test_accs.append(test_acc)
