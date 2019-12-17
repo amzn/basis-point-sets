afe05014092083237d2426b0644e2dfb26892b1a
Prokudin <prokus@a483e705f5d5.ant.amazon.com>
2019-12-17 14:35:22 +0100

    readme updated

--- bps_demos/train_modelnet_conv3d.py
+++ bps_demos/train_modelnet_conv3d.py
@@ -179,7 +179,7 @@ def main():
 
     optimizer = pt.optim.Adam(model.parameters(), lr=1e-3)
 
-    n_epochs = 1300
+    n_epochs = 2000
     pbar = range(0, n_epochs)
     test_accs = []
     test_losses = []
@@ -194,6 +194,10 @@ def main():
             for param_group in optimizer.param_groups:
                 print("decreasing the learning rate to 1e-4..")
                 param_group['lr'] = 1e-4
+        if epoch_idx == 1500:
+            for param_group in optimizer.param_groups:
+                print("decreasing the learning rate to 1e-4..")
+                param_group['lr'] = 1e-5
         if epoch_idx % 10 == 0:
             test_loss, test_acc = test(model, DEVICE, test_loader, epoch_idx)
             test_accs.append(test_acc)
