ef5905bff243725e485de106e1ad61794dbd9200
Prokudin <prokus@a483e705f5d5.ant.amazon.com>
2019-12-19 13:48:56 +0100

    final training pipeline

--- bps_demos/train_modelnet_conv3d.py
+++ bps_demos/train_modelnet_conv3d.py
@@ -179,7 +179,7 @@ def main():
 
     optimizer = pt.optim.Adam(model.parameters(), lr=1e-3)
 
-    n_epochs = 1200
+    n_epochs = 300
     pbar = range(0, n_epochs)
     test_accs = []
     test_losses = []
@@ -190,7 +190,7 @@ def main():
     start = time.time()
     for epoch_idx in pbar:
         fit(model, DEVICE, train_loader, optimizer)
-        if epoch_idx == 1000:
+        if epoch_idx == 250:
             for param_group in optimizer.param_groups:
                 print("decreasing the learning rate to 1e-4..")
                 param_group['lr'] = 1e-4
