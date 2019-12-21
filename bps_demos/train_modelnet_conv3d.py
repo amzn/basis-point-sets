019a871c36f057b5ad1b0c58cbd6e7805988b4cb
Prokudin <prokus@a483e705f5d5.ant.amazon.com>
2019-12-21 22:48:08 +0100

    final training pipeline

--- bps_demos/train_modelnet_conv3d.py
+++ bps_demos/train_modelnet_conv3d.py
@@ -66,7 +66,7 @@ class ShapeClassifierConv3D(nn.Module):
         self.fc2 = nn.Linear(in_features=2048, out_features=512)
         self.bn2 = nn.BatchNorm1d(512)
         self.do3 = nn.Dropout(0.6)
-        self.fc3 = nn.Linear(in_features=1024, out_features=n_classes)
+        self.fc3 = nn.Linear(in_features=512, out_features=n_classes)
 
     def forward(self, x):
 
